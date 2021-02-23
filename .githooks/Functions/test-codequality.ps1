c386510826193e1adaeea6b58b9e32aafa0cb345
Mark Evans <mark@madspaniels.co.uk>
2021-02-23 19:19:25 +0000

    feat(Hooks): :sparkles: Added configuration to pre-commit

--- .githooks/Functions/test-codequality.ps1
+++ .githooks/Functions/test-codequality.ps1
@@ -1,9 +1,13 @@
 function test-codequality {
+    param(
+        $severity
+    )
+    if (!($severity)){$severity='Information'}
     Import-Module -Name PSScriptAnalyzer -Force
     $output = @()
     foreach ($file in (get-commitfiles | Where-Object { $_.EndsWith('.ps1') -or $_.EndsWith('.psm1') -or $_.EndsWith('.psd1') })) {
         Write-Log -Level DEBUG -Message 'Analyzing {0}' -Arguments $file
-        $output += Invoke-ScriptAnalyzer -Path $file
+        $output += Invoke-ScriptAnalyzer -Path $file -Severity $severity
     }
     if ($output.Count -ne 0) {
         Write-Log -Level ERROR -Message  "Basic scripting errors were found in updated scripts. fix or use git commit --no-verify"
