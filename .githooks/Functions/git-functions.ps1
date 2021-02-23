e7f0b573c7ac182b344b3e7de38f971cd5e15ab7
Mark Evans <mark@madspaniels.co.uk>
2021-02-23 11:43:07 +0000

    feat(Hook-Functions): :sparkles: test-isbinary
    Uses git diff to check if file is binary

--- .githooks/Functions/git-functions.ps1
+++ .githooks/Functions/git-functions.ps1
@@ -1,3 +1,10 @@
 function get-commitfiles {
     Return (git diff-index --name-only --diff-filter=ACM --cached HEAD --)    
 }
+
+function test-isbinary {
+    param (
+        $filename
+    )
+    return (git diff --no-index --numstat nul $filename).StartsWith("-`t-`t")
+}
